90e709cfbceb84ea8f644a9a757866f7fbefe040
ktritsh7 <ktriesh7@gmail.com>
2021-05-13 00:17:06 +0530

    Added:Code for checking first name validity

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,9 +1,33 @@
 package com.bridgelabz.userregistrationproblem;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
+	
+	public static boolean isValidFirstName(String firstname) {
+		
+		String regex = "^[A-Z][a-z]{2,}";
+		
+        Pattern p = Pattern.compile(regex);
+        
+        if (firstname == null) {
+            return false;
+        }
+        
+        Matcher m = p.matcher(firstname);
+        return m.matches();
+	}
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to user registration problem.");
+		
+		// Test Case 
+        String name1 = "Ritesh";
+        System.out.println( name1 + " : " + isValidFirstName(name1));
+        
+		// Test Case 
+        String name2 = "ritesh";
+        System.out.println( name2 + " : " + isValidFirstName(name2));
 
 	}
 
