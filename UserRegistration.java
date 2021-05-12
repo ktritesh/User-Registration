4b4aceef73d94b3c1382046f9492d3178c841fd6
ktritsh7 <ktriesh7@gmail.com>
2021-05-13 00:40:06 +0530

    Added:Code for checking user email validity

--- UserRegistration.java
+++ UserRegistration.java
@@ -6,7 +6,6 @@ import java.util.regex.Pattern;
 public class UserRegistration {
 	
 	public static boolean isValidFirstName(String firstname) {
-		
 		String regex = "^[A-Z][a-z]{2,}";
         Pattern p = Pattern.compile(regex);
         Matcher m = p.matcher(firstname);
@@ -14,10 +13,16 @@ public class UserRegistration {
 	}
 	
 	public static boolean isValidLastName(String lastname) {
-		
 		String regex = "^[A-Z][a-z]{2,}$";
         Pattern p = Pattern.compile(regex);
         Matcher m = p.matcher(lastname);
+        return m.matches();
+	}
+	
+	public static boolean isValidEmail(String emailid) {
+		String regex = "^[a-zA-Z][a-zA-Z0-9_\\-+]*[.]{0,1}[a-zA-Z0-9_\\-+]{1,}[@][a-zA-Z0-9]{1,}[.][a-zA-Z]{2,}[.]{0,}[a-zA-Z]*$";
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(emailid);
         return m.matches();
 	}
 
@@ -34,6 +39,9 @@ public class UserRegistration {
         
         String lastName2 = "tiwari";
         System.out.println( lastName2 + " : " + isValidLastName(lastName2));
+        
+        String emailId1 = "abc.xyz@bl.co.in";
+        System.out.println( emailId1 + " : " + isValidLastName(emailId1));
 
 	}
 
